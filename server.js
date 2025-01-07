81882aabb47503993781e2d2ffc439a0da2bac56
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-01-07 12:02:19 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+  });
+
+  socket.on('disconnect', () => {
+    let disconnectedUser;
+    users.forEach((value, key) => {
+      if (value === socket.id) disconnectedUser = key;
+    });
+    users.delete(disconnectedUser);
+    io.emit('users-update', Array.from(users.keys()));
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
