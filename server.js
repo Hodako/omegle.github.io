a9f9aac2c34f2d44157626c542145cc2f729874d
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-01-07 12:07:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,45 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Store active users and their socket IDs
+const users = new Map();
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join', (userId) => {
+    users.set(userId, socket.id);
+    io.emit('users-update', Array.from(users.keys()));
+  });
+
+  socket.on('call-user', ({ to, signal, from }) => {
+    io.to(users.get(to)).emit('incoming-call', {
+      signal,
+      from
+    });
+  });
+
+  socket.on('answer-call', ({ to, signal }) => {
+    io.to(users.get(to)).emit('call-accepted', signal);
+  });
+
+  socket.on('send-message', ({ to, message }) => {
+    io.to(users.get(to)).emit('receive-message', {
+      from: socket.id,
+      message
+    });
+  });
 
   socket.on('disconnect', () => {
     let disconnectedUser;
